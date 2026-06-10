eb7a3b2079af7d8851ec679c456cf0acfcebc6f7
Alexander Holte-Davidsen <alexander.holte.davidsen@kulturit.no>
2026-06-10 22:24:34 +0200

    Update overlay.nix hashes for v1.3.5

--- overlay.nix
+++ overlay.nix
@@ -5,19 +5,19 @@ let
   assets = {
     x86_64-linux = {
       url = "https://github.com/taskbook-sh/taskbook/releases/download/v${version}/tb-linux-x86_64.tar.gz";
-      hash = "sha256-qRV2oF1yIF9rM9wseCjrN/iRCnMBfZAZwIEC0rxUJJA=";
+      hash = "sha256-zGZ/4UebIvVv7kTAoMw0i0/3gRDi3C4kXrK+7frkwtk=";
     };
     aarch64-linux = {
       url = "https://github.com/taskbook-sh/taskbook/releases/download/v${version}/tb-linux-aarch64.tar.gz";
-      hash = "sha256-G4mhKoG218Odayh8VkMgTv9Snij9hzzZcwE2qLK4iGI=";
+      hash = "sha256-d0X8L5wSglOB3wE4pL1JkFzybkTyLjzio5bSn361y1o=";
     };
     x86_64-darwin = {
       url = "https://github.com/taskbook-sh/taskbook/releases/download/v${version}/tb-darwin-x86_64.tar.gz";
-      hash = "sha256-liGvtkXz5VWn3IabH2GU9NhpddsN3ruX86PZWAlsErw=";
+      hash = "sha256-o8CYGUHpJa43gzlB3mnanJFTOwib1BJM4HtCCWmIH4w=";
     };
     aarch64-darwin = {
       url = "https://github.com/taskbook-sh/taskbook/releases/download/v${version}/tb-darwin-aarch64.tar.gz";
-      hash = "sha256-nLfwAV/l1qRhxpA++6PdiW1D+YByGQTa68YRV1rcjDQ=";
+      hash = "sha256-csEyv3XJg8j53xSjYqWJnjSNflu7BO8cYT1oWKdKCOo=";
     };
   };
 
